0938bd44139ec60d1dbfe860534652577929582c
zhushibao <nicozhushibao@qq.com>
2020-07-05 16:48:22 +0800

    1.今日力扣算法题 2.jmh比较if和switch的性能，使用Javap分析为什么switch性能好

--- hello-learn/src/main/java/com/soranico/arithmetic/simple/BitOperation.java
+++ hello-learn/src/main/java/com/soranico/arithmetic/simple/BitOperation.java
@@ -46,30 +46,49 @@ public class BitOperation {
      * 136
      * 给定一个非空整数数组，除了某个元素只出现一次以外，
      * 其余每个元素均出现两次。找出那个只出现了一次的元素。
+     *
      * @param nums
      * @return
      */
     public static int singleNumber(int[] nums) {
-        int result=0;
+        int result = 0;
         for (int num : nums) {
             /**
              * a^(b^c)=(a^b)^c 所以位置不影响结果
              * a^a=0 将所有数全部异或一遍，其余成对的均为0
              * 0^a=a 唯一的那个数还是那个数
              */
-            result^=num;
+            result ^= num;
         }
         return result;
     }
 
 
-
-
-
+    /**
+     * 693
+     * 给定一个正整数，检查他是否为交替位二进制数：换句话说，
+     * 就是他的二进制数相邻的两个位数永不相等。
+     * <p>
+     * 假设数字n的二进制为
+     * 0101 0101 0101 0101
+     * 右移1位
+     * 0010 1010 1010 1010
+     * 异或后
+     * 0111 1111 1111 1111
+     * 则+1必为2的幂
+     * @param n
+     * @return
+     */
+    public static boolean hasAlternatingBits(int n) {
+        int powerOfTwo = ((n >> 1) ^ n) + 1;
+        return Integer.bitCount(powerOfTwo)==1;
+    }
 
 
     public static void main(String[] args) {
 
+        System.err.println(0b01110111);
+        System.err.println(hasAlternatingBits(0b01110111));
     }
 
 
